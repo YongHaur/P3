fe09ae0fdef45204b38c6e902dd255c97ea18896
Yong Haur <tayyh@utar.edu.my>
2017-07-12 14:35:01 +0800

    This is the first commit

---
+++ P2/P2.cpp
@@ -0,0 +1,13 @@
+// P2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+
+	printf("Hello World! \n\n\n This is SCC Practical 2! \n\n\n");
+    return 0;
+}
+
